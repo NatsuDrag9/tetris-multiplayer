3a3eaca9a51abba0d1c5e325d91a7578f7b07a73
Natsu <rohit@savart.com>
2024-04-07 20:51:20 +0200

    Created a common game area

---
+++ frontend/src/components/GameArea/GameArea.tsx
@@ -0,0 +1,12 @@
+import './GameArea.scss';
+
+function GameArea() {
+  return (
+    <div className="game-area">
+      <div className="game-area__game">This is where tetrominoes will fall</div>
+      <div className="game-area__info">Game information is displayed</div>
+    </div>
+  );
+}
+
+export default GameArea;
--- frontend/src/pages/SinglePlayer/SinglePlayer.tsx
+++ frontend/src/pages/SinglePlayer/SinglePlayer.tsx
@@ -1,7 +1,12 @@
+import GameArea from '@components/GameArea/GameArea';
 import './SinglePlayer.scss';
 
 function SinglePlayer() {
-  return <div className="single-player">Hello from single player</div>;
+  return (
+    <div className="single-player">
+      <GameArea />
+    </div>
+  );
 }
 
 export default SinglePlayer;
