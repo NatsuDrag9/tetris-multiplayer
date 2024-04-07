15dc9ad1f78c52e097d24107f6179c9e69ee9346
Natsu <rohit@savart.com>
2024-04-07 13:30:24 +0200

    Created home page with styles and added routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,18 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.scss';
 import Home from '@pages/Home/Home';
+import MultiPlayer from '@pages/MultiPlayer/MultiPlayer';
+import SinglePlayer from '@pages/SinglePlayer/SinglePlayer';
 
 function App() {
   return (
     <div className="app">
-      <Home />
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/multiplayer" element={<MultiPlayer />} />
+        <Route path="/singleplayer" element={<SinglePlayer />} />
+      </Routes>
     </div>
   );
 }
--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,9 +1,24 @@
+import { useNavigate } from 'react-router-dom';
 import './Home.scss';
 
 function Home() {
+  const navigate = useNavigate();
+  const handleSinglePlayerClick = () => {
+    navigate('/singleplayer');
+  };
+
+  const handleMultiPlayerClick = () => {
+    navigate('/multiplayer');
+  };
   return (
     <div className="home">
-      <h3 className="home__title">Welcome to Tetris Arcade. Select a mode</h3>
+      <h3 className="home__title">
+        Welcome to Tetris Arcade. Select game mode
+      </h3>
+      <div className="home__body">
+        <button onClick={handleSinglePlayerClick}>Single Player</button>
+        <button onClick={handleMultiPlayerClick}>Multi Player</button>
+      </div>
     </div>
   );
 }
