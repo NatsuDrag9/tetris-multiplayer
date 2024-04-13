a1e319b2229be479a929529be97ac49ceac06a7a
Natsu <rohit@savart.com>
2024-04-13 21:50:35 +0200

    Added 404 route

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,5 +1,5 @@
-import express from 'express';
-import getRandomCode from '@routes/getRandomCode';
+import express, { Request, Response, NextFunction } from 'express';
+import getRandomCode from '@src/routes/getRandomCode';
 import serverMiddlewares from './middlewares/serverMiddlewares';
 
 const app = express();
@@ -14,5 +14,20 @@ app.get('/', (req, res) => {
 
 // Application routes
 app.use('/api', getRandomCode);
+
+// Catch-all route
+app.use((req, res, next) => {
+  const error = new Error('Page Not Found');
+  res.status(404);
+  next(error);
+});
+
+app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
+  if (res.statusCode === 404) {
+    res.json({ error: 'Page Not Found' });
+  } else {
+    next(error);
+  }
+});
 
 export default app;
