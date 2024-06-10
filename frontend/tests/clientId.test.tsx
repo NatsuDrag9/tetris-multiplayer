ce7d9fb4d01797637e107c9e771e6b556b2f01b6
NatsuDrag9 <rohitimandi9@zoho.com>
2024-06-10 22:37:52 +0200

    Completed multiplayerclientid tests

--- frontend/tests/clientId.test.tsx
+++ frontend/tests/clientId.test.tsx
@@ -3,43 +3,50 @@ import {
   assertType,
   beforeAll,
   afterEach,
-  afterAll,
   describe,
   expect,
 } from 'vitest';
-import { fireEvent, render, waitFor } from '@testing-library/react';
+import { fireEvent, waitFor, cleanup } from '@testing-library/react';
+import render from './setupTests';
 import { fetchClientId } from '@services/multiPlayer';
 import { logInTest } from '@utils/log-utils';
-import { successfulClientIdServer, errorClientIdServer } from './server';
 import MultiplayerClientId from '@pages/MultiplayerClientId/MultiplayerClientId';
 import { WebSocketProvider } from '@contexts/WebSocketContext';
 import { MemoryRouter } from 'react-router-dom';
+import axios from 'axios';
+import MockAdapter from 'axios-mock-adapter';
+import ENDPOINTS from '@constants/apiEndpoints';
+
+const mockAxios = new MockAdapter(axios);
+const API_BASE_URL = import.meta.env.VITE_BASE_URL;
 
 describe('Multiplayer Client Id flow', () => {
-  beforeAll(() => successfulClientIdServer.listen());
+  beforeAll(() => mockAxios.resetHandlers());
   afterEach(() => {
-    successfulClientIdServer.resetHandlers();
-    errorClientIdServer.resetHandlers();
-    localStorage.removeItem('clientId');
-  });
-  afterAll(() => {
-    successfulClientIdServer.close();
-    errorClientIdServer.close();
+    mockAxios.resetHandlers();
+    localStorage.clear();
+    cleanup;
   });
 
   // Initial test
   test('local storage should not have a clientId initially', () => {
-    expect(localStorage.getItem('clientId')).toBe(null);
+    expect(localStorage.getItem('clientId')).toBeNull();
   });
 
   // Test for 200 API response
   test('Sucessful API response should be of type JSON', async () => {
+    mockAxios.onGet(`${API_BASE_URL}/${ENDPOINTS.CLIENT_ID}`).reply(200, {
+      clientId: 'testingCliendId123',
+    });
     const response = await fetchClientId();
     logInTest(response);
     assertType<'json'>(response);
   });
 
   test('Set clientId in local storage on success API response', async () => {
+    mockAxios.onGet(`${API_BASE_URL}/${ENDPOINTS.CLIENT_ID}`).reply(200, {
+      clientId: 'testingClientId123',
+    });
     const { getByTestId } = render(
       <MemoryRouter initialEntries={['/']}>
         <WebSocketProvider>
@@ -54,17 +61,16 @@ describe('Multiplayer Client Id flow', () => {
       expect(getByTestId('display-ticket').innerHTML).toMatch(
         'Your ticket: testingClientId123'
       );
+      expect(localStorage.getItem('clientId')).toBe('testingClientId123');
     });
-
-    expect(localStorage.getItem('clientId')).toBe('testingClientId123');
   });
 
-  // Test for handling non-200 API response
-  test('Handles Cliend Id API error response', async () => {
-    // Start the error server
-    errorClientIdServer.listen();
+  // Test for handling error API response
+  test('Client Id is null and get-ticket button is rendered', async () => {
+    // This statement is not required as localStorage.clear() removes "clienId" in afterEach
+    // localStorage.removeItem('clientId');
 
-    const { getByTestId } = render(
+    const { queryByTestId } = render(
       <MemoryRouter initialEntries={['/']}>
         <WebSocketProvider>
           <MultiplayerClientId />
@@ -72,10 +78,33 @@ describe('Multiplayer Client Id flow', () => {
       </MemoryRouter>
     );
 
-    fireEvent.click(getByTestId('get-ticket'));
+    await waitFor(() => {
+      expect(queryByTestId('get-ticket')).toBeDefined();
+      expect(queryByTestId('display-ticket')).toBeNull();
+    });
+  });
+
+  test('Handles Client Id API error response', async () => {
+    mockAxios.onGet(`${API_BASE_URL}/${ENDPOINTS.CLIENT_ID}`).reply(500, {
+      message: 'Error generating client ID',
+    });
+    const { getByTestId, queryByTestId } = render(
+      <MemoryRouter initialEntries={['/']}>
+        <WebSocketProvider>
+          <MultiplayerClientId />
+        </WebSocketProvider>
+      </MemoryRouter>
+    );
+
+    // Ensure 'get-ticket' is available and clickable
+    await waitFor(() => {
+      expect(queryByTestId('get-ticket')).not.toBeNull();
+      fireEvent.click(getByTestId('get-ticket'));
+    });
 
     await waitFor(() => {
-      expect(localStorage.getItem('clientId')).toBe(null);
+      expect(localStorage.getItem('clientId')).toBeNull();
+      expect(queryByTestId('display-ticket')).toBeNull();
     });
   });
 });
