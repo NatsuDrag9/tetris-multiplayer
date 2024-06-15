28327cc57bb0a2691ecbf73124ba2342be5c775c
NatsuDrag9 <rohitimandi9@zoho.com>
2024-06-15 19:00:18 +0200

    Finished cientId tests

--- frontend/tests/clientId.test.tsx
+++ frontend/tests/clientId.test.tsx
@@ -5,6 +5,7 @@ import {
   afterEach,
   describe,
   expect,
+  vi,
 } from 'vitest';
 import { fireEvent, waitFor, cleanup } from '@testing-library/react';
 import render from './setupTests';
@@ -13,24 +14,28 @@ import { logInTest } from '@utils/log-utils';
 import MultiplayerClientId from '@pages/MultiplayerClientId/MultiplayerClientId';
 import { WebSocketProvider } from '@contexts/WebSocketContext';
 import { MemoryRouter } from 'react-router-dom';
-import axios from 'axios';
+import axios, { AxiosError } from 'axios';
 import MockAdapter from 'axios-mock-adapter';
 import ENDPOINTS from '@constants/apiEndpoints';
+import toast from 'react-hot-toast';
 
 const mockAxios = new MockAdapter(axios);
 const API_BASE_URL = import.meta.env.VITE_BASE_URL;
+
+// const spyOnToastError = vi.spyOn(toast, 'error');
 
 describe('Multiplayer Client Id flow', () => {
   beforeAll(() => mockAxios.resetHandlers());
   afterEach(() => {
     mockAxios.resetHandlers();
-    localStorage.clear();
+    sessionStorage.clear();
+    vi.restoreAllMocks();
     cleanup;
   });
 
   // Initial test
   test('local storage should not have a clientId initially', () => {
-    expect(localStorage.getItem('clientId')).toBeNull();
+    expect(sessionStorage.getItem('clientId')).toBeNull();
   });
 
   // Test for 200 API response
@@ -61,14 +66,14 @@ describe('Multiplayer Client Id flow', () => {
       expect(getByTestId('display-ticket').innerHTML).toMatch(
         'Your ticket: testingClientId123'
       );
-      expect(localStorage.getItem('clientId')).toBe('testingClientId123');
+      expect(sessionStorage.getItem('clientId')).toBe('testingClientId123');
     });
   });
 
   // Test for handling error API response
   test('Client Id is null and get-ticket button is rendered', async () => {
-    // This statement is not required as localStorage.clear() removes "clienId" in afterEach
-    // localStorage.removeItem('clientId');
+    // This statement is not required as sessionStorage.clear() removes "clienId" in afterEach
+    // sessionStorage.removeItem('clientId');
 
     const { queryByTestId } = render(
       <MemoryRouter initialEntries={['/']}>
@@ -84,10 +89,26 @@ describe('Multiplayer Client Id flow', () => {
     });
   });
 
-  test('Handles Client Id API error response', async () => {
+  test('Test client Id API server error response', async () => {
     mockAxios.onGet(`${API_BASE_URL}/${ENDPOINTS.CLIENT_ID}`).reply(500, {
       message: 'Error generating client ID',
     });
+
+    try {
+      await axios.get(`${API_BASE_URL}/${ENDPOINTS.CLIENT_ID}`);
+    } catch (error) {
+      if (error instanceof AxiosError && error.response) {
+        expect(error.response.data.message).toBe('Error generating client ID');
+        expect(error.response.status).toBe(500);
+      }
+    }
+  });
+
+  test('DOM update on client Id API server error response', async () => {
+    mockAxios.onGet(`${API_BASE_URL}/${ENDPOINTS.CLIENT_ID}`).reply(500, {
+      message: 'Error generating client ID',
+    });
+
     const { getByTestId, queryByTestId } = render(
       <MemoryRouter initialEntries={['/']}>
         <WebSocketProvider>
@@ -96,15 +117,63 @@ describe('Multiplayer Client Id flow', () => {
       </MemoryRouter>
     );
 
-    // Ensure 'get-ticket' is available and clickable
-    await waitFor(() => {
-      expect(queryByTestId('get-ticket')).not.toBeNull();
-      fireEvent.click(getByTestId('get-ticket'));
-    });
+    expect(queryByTestId('get-ticket')).not.toBeNull();
+    fireEvent.click(getByTestId('get-ticket'));
 
     await waitFor(() => {
-      expect(localStorage.getItem('clientId')).toBeNull();
+      expect(sessionStorage.getItem('clientId')).toBeNull();
       expect(queryByTestId('display-ticket')).toBeNull();
+      // expect(spyOnToastError).toHaveBeenCalledWith(
+      //   'Error generating client ID'
+      // );
     });
   });
+
+  // test('Test client Id API network error response', async () => {
+  //   mockAxios.onGet(`${API_BASE_URL}/${ENDPOINTS.CLIENT_ID}`).networkError();
+
+  //   try {
+  //     await axios.get(`${API_BASE_URL}/${ENDPOINTS.CLIENT_ID}`);
+  //   } catch (error) {
+  //     // Type guard
+  //     if (error instanceof Error) {
+  //       /*
+  //         networkError returns an "error" which is an instanceOf Error and, not AxiosError
+  //        */
+
+  //       expect(error).toBeInstanceOf(AxiosError); // Check if error is an instance of AxiosError
+  //       expect(error.isAxiosError).toBeTruthy(); // Confirm it's an Axios-specific error
+  //       expect(error.request).toBeTruthy(); // There should be a request defined since an attempt was made
+  //       expect(error.response).toBeUndefined(); // Response should be undefined in a network error
+  //     } else {
+  //       throw new Error('Caught error is not an instance of AxiosError');
+  //     }
+  //   }
+  // });
+
+  // test('Handles Client Id API network error response', async () => {
+  //   mockAxios.onGet(`${API_BASE_URL}/${ENDPOINTS.CLIENT_ID}`).networkError();
+
+  //   const { getByTestId, queryByTestId } = render(
+  //     <MemoryRouter initialEntries={['/']}>
+  //       <WebSocketProvider>
+  //         <MultiplayerClientId />
+  //       </WebSocketProvider>
+  //     </MemoryRouter>
+  //   );
+
+  //   await waitFor(() => {
+  //     expect(queryByTestId('get-ticket')).not.toBeNull();
+  //     fireEvent.click(getByTestId('get-ticket'));
+  //   });
+
+  //   await waitFor(() => {
+  //     expect(sessionStorage.getItem('clientId')).toBeNull();
+  //     expect(queryByTestId('display-ticket')).toBeNull();
+  //     // logInTest('Calls to toast.error', spyOnToastError.mock.calls);
+  //     // expect(spyOnToastError).toHaveBeenCalledWith(
+  //     //   'Server is offline or unreachable'
+  //     // );
+  //   });
+  // });
 });
