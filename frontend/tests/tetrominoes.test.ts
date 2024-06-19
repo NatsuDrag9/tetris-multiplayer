68748be5023685f2fb98efe08104bba4fcd1b14b
NatsuDrag9 <rohitimandi9@zoho.com>
2024-06-19 14:14:10 +0200

    Removed tetromino test file

--- frontend/tests/tetrominoes.test.ts
+++
@@ -1,8 +0,0 @@
-import { test, assertType } from 'vitest';
-import getRandomTetromino from '@utils/get-random-tetromino';
-import { Tetromino } from '@customTypes/tetromonoTypes';
-
-test('A random tetromino has return type "Tetrominoes"', () => {
-  const randomTetromino = getRandomTetromino();
-  assertType<Tetromino>(randomTetromino);
-});
